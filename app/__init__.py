c7cd510a3b6e3e59ed8858269fdf2ca63bbaba2d
wuyinlei <wuyinlei19931118@hotmail.com>
2018-03-09 16:34:43 +0800

    管理员界面搭建

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf8
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,3 +11,8 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
--- app/home/views.py
+++ app/home/views.py
@@ -61,3 +61,5 @@ def search():
 @home.route("/play/")
 def play():
     return render_template("home/play.html")
+
+
